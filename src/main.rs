c1e7ffb0c1bea66dc1db1f0bb9964bee3ccd60d7
JuanGualim <gua24852@uvg.edu.gt>
2026-08-25 00:15:44 -0600

    Se dibujó el poligono 1 como una estrella

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,53 @@ use vertex::Vertex;
 use raylib::prelude::*;
 
 fn main() {
+    // Tamaño de la imagen
     let width = 800;
     let height = 600;
 
+    // Crear framebuffer
     let mut framebuffer = Framebuffer::new(width, height);
-    
+
+    // Fondo blanco
     framebuffer.set_background_color(Color::WHITE);
     framebuffer.clear();
+
+    // =========================================================
+    // POLIGONO 1
+    // =========================================================
+
+    let polygon1 = vec![
+        Vertex::new(165, 380),
+        Vertex::new(185, 360),
+        Vertex::new(180, 330),
+        Vertex::new(207, 345),
+        Vertex::new(233, 330),
+        Vertex::new(230, 360),
+        Vertex::new(250, 380),
+        Vertex::new(220, 385),
+        Vertex::new(205, 410),
+        Vertex::new(193, 383),
+    ];
+
+    // Primero rellenamos el poligono
+    framebuffer.set_current_color(Color::GREEN);
+
+    fill_polygon(
+        &mut framebuffer,
+        &polygon1,
+    );
+
+    // Luego dibujamos el borde encima
+    framebuffer.set_current_color(Color::BLACK);
+
+    draw_polygon(
+        &mut framebuffer,
+        &polygon1,
+    );
+
+    // =========================================================
+    // POLIGONO 3
+    // =========================================================
 
     let polygon3 = vec![
         Vertex::new(377, 249),
@@ -24,7 +64,7 @@ fn main() {
         Vertex::new(436, 249),
     ];
 
-    // Primero rellenamos
+    // Relleno azul
     framebuffer.set_current_color(Color::BLUE);
 
     fill_polygon(
@@ -32,7 +72,7 @@ fn main() {
         &polygon3,
     );
 
-    // Después dibujamos el borde
+    // Borde negro
     framebuffer.set_current_color(Color::BLACK);
 
     draw_polygon(
@@ -40,7 +80,11 @@ fn main() {
         &polygon3,
     );
 
+    // =========================================================
+    // GUARDAR RESULTADO
+    // =========================================================
+
     framebuffer.render_to_file("out.bmp");
 
-    println!("Imagen generada.");
+    println!("Imagen generada correctamente en out.bmp");
 }
